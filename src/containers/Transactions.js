a5ea9f88fc2d510287b8334f13ed13cb04762e0e
WillPilgrim <sean.turner@iinet.net.au>
2018-07-16 22:01:16 +0800

    Adjust viewport to fit whole number of rows

--- src/containers/Transactions.js
+++ src/containers/Transactions.js
@@ -220,7 +220,7 @@ export default class Transactions extends Component {
   render() {
     let h =
       Math.max(document.documentElement.clientHeight, window.innerHeight || 0) -
-      290;
+      284;
     let divStyle = { boxSizing: "border-box", height: `${h}px` };
     let data = [];
     let currAcc;
@@ -331,27 +331,3 @@ export default class Transactions extends Component {
     );
   }
 }
-
-// function getDatePicker() {
-//   function Datepicker() {}
-//   Datepicker.prototype.init = function(params) {
-//     this.eInput = document.createElement("input");
-//     this.eInput.value = params.value;
-//     $(this.eInput).datepicker({ dateFormat: "dd/mm/yy" });
-//   };
-//   Datepicker.prototype.getGui = function() {
-//     return this.eInput;
-//   };
-//   Datepicker.prototype.afterGuiAttached = function() {
-//     this.eInput.focus();
-//     this.eInput.select();
-//   };
-//   Datepicker.prototype.getValue = function() {
-//     return this.eInput.value;
-//   };
-//   Datepicker.prototype.destroy = function() {};
-//   Datepicker.prototype.isPopup = function() {
-//     return false;
-//   };
-//   return Datepicker;
-// }
